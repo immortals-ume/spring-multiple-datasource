96145060ff3ebbd4f0b8d32f6a591bde9cd1878b
immortals-ume <kaish127799@outlook.com>
2022-05-03 20:33:20 +0530

    provided assertion for the default test class

--- src/test/java/com/immortals/springmultipledatasource/SpringMultipleDatasourceApplicationTests.java
+++ src/test/java/com/immortals/springmultipledatasource/SpringMultipleDatasourceApplicationTests.java
@@ -1,12 +1,27 @@
 package com.immortals.springmultipledatasource;
 
+import com.immortals.springmultipledatasource.controller.CustomerController;
+import com.immortals.springmultipledatasource.controller.CustomerUpdateController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest class SpringMultipleDatasourceApplicationTests{
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class SpringMultipleDatasourceApplicationTests{
 
 
-    @Test void contextLoads(){
+    @Autowired
+    private CustomerController customerController;
+
+    @Autowired
+    private CustomerUpdateController customerUpdateController;
+
+    @Test
+    void contextLoads(){
+        assertNotNull( customerController );
+        assertNotNull( customerUpdateController );
     }
 
 }
